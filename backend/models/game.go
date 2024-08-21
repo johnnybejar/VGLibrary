555115d499ac917c385eb52ab97a105c5d2c7def
johnnyb <jbejar2001@yahoo.com>
2024-08-21 18:22:02 -0400

    removed more unused fields from Game struct

--- backend/models/game.go
+++ backend/models/game.go
@@ -5,21 +5,15 @@ type Game struct {
 	AggregatedRating      float32 `json:"aggregated_rating"`
 	AggregatedRatingCount int     `json:"aggregated_rating_count"`
 	AlternativeNames      []int   `json:"alternative_names"`
+	Collections           []int   `json:"collections"` // Refers to franchise of the game
 	Cover                 int     `json:"cover"`
-	ExternalGames         []int   `json:"external_games"`
-	Franchise             string  `json:"franchise"`
-	FirstReleaseDate      int     `json:"first_release_date"`
-	GameEngines           []int   `json:"game_engines"`
 	GameModes             []int   `json:"game_modes"`
 	Genres                []int   `json:"genres"`
 	InvolvedCompanies     []int   `json:"involved_companies"`
 	Name                  string  `json:"name"`
 	Platforms             []int   `json:"platforms"`
-	ReleaseDates          []int   `json:"release_dates"`
+	ReleaseDate           int     `json:"first_release_date"`
 	Slug                  string  `json:"slug"`
 	Summary               string  `json:"summary"`
-	UpdatedAt             int     `json:"updated_at"`
 	Url                   string  `json:"url"`
-	Websites              []int   `json:"websites"`
-	Checksum              string  `json:"checksum"`
 }
