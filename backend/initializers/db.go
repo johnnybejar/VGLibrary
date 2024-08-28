df28de718b7b10122e2a8298ce67a3e4a4ad801b
johnnyb <jbejar2001@yahoo.com>
2024-08-28 19:01:56 -0400

    init db connection

--- backend/initializers/db.go
+++ backend/initializers/db.go
@@ -1,11 +1,29 @@
 package initializers
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func DB() {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+var DB *gorm.DB
+
+func InitDBConn() {
+	username := os.Getenv("DBUSER")
+	password := os.Getenv("DBPASSWORD")
+	address := os.Getenv("DBADDRESS")
+	port := os.Getenv("DBPORT")
+	dbname := os.Getenv("DBNAME")
+
+	var err error
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, address, port, dbname)
+	fmt.Println(dsn)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to db")
+	}
 }
--- backend/main.go
+++ backend/main.go
@@ -20,7 +20,7 @@ import (
 
 func init() {
 	initializers.LoadEnvVariables()
-	initializers.DB()
+	initializers.InitDBConn()
 }
 
 func main() {
