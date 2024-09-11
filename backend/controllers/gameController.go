b2af47891572219c13efefc641857d274753b763
johnnyb <jbejar2001@yahoo.com>
2024-09-11 15:09:07 -0400

    removed generic igdb route handler

--- backend/controllers/gameController.go
+++ backend/controllers/gameController.go
@@ -53,7 +53,7 @@ func GetAccessToken(c *gin.Context) {
 	c.JSON(http.StatusOK, body)
 }
 
-func GetIGDBRoute (c *gin.Context, url string, fields string, model *[]interface{}) {
+func GetGame(c *gin.Context) {
 	CLIENT_ID := os.Getenv("TWITCH_CLIENT_ID")
 	bearer, err := c.Cookie("IGDBAccessToken")
 	if err != nil {
@@ -69,7 +69,8 @@ func GetIGDBRoute (c *gin.Context, url string, fields string, model *[]interface
 		log.Fatal(err)
 	}
 
-	reqBody := fmt.Sprintf("%s; where id = %s;", fields, string(jsonData))
+	url := "https://api.igdb.com/v4/games"
+	reqBody := fmt.Sprintf("fields id, aggregated_rating, aggregated_rating_count, alternative_names, collections, cover, game_modes, genres, involved_companies, name, platforms, first_release_date, slug, summary, url; where id = %s;", string(jsonData))
 
 	reader := bytes.NewReader([]byte(reqBody))
 
@@ -89,6 +90,8 @@ func GetIGDBRoute (c *gin.Context, url string, fields string, model *[]interface
 	}
 
 	defer res.Body.Close()
+
+	var game []models.Game
 	
 	switch {
 		case res.StatusCode == 401:
@@ -100,83 +103,15 @@ func GetIGDBRoute (c *gin.Context, url string, fields string, model *[]interface
 				"error": "Bad request",
 			})
 		default:
-			err := json.NewDecoder(res.Body).Decode(model)
+			err := json.NewDecoder(res.Body).Decode(&game)
 			if err != nil {
 				log.Fatal(err)
 			}
 
 			c.JSON(http.StatusOK, gin.H{
-				"response": (*model)[0],
+				"response": game[0],
 			})
 	}
-}
-
-func GetGame(c *gin.Context) {
-	// CLIENT_ID := os.Getenv("TWITCH_CLIENT_ID")
-	// bearer, err := c.Cookie("IGDBAccessToken")
-	// if err != nil {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{
-	// 		"error": "Not authorized",
-	// 	})
-	// }
-
-	// bearer = "Bearer " + bearer
-
-	// jsonData, err := io.ReadAll(c.Request.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// url := "https://api.igdb.com/v4/games"
-	// reqBody := fmt.Sprintf("fields id, aggregated_rating, aggregated_rating_count, alternative_names, collections, cover, game_modes, genres, involved_companies, name, platforms, first_release_date, slug, summary, url; where id = %s;", string(jsonData))
-
-	// reader := bytes.NewReader([]byte(reqBody))
-
-	// req, err := http.NewRequest("POST", url, reader)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// req.Header.Set("Client-ID", CLIENT_ID)
-	// req.Header.Add("Authorization", bearer)
-
-	// client := &http.Client{Timeout: 10 * time.Second}
-
-	// res, err := client.Do(req)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer res.Body.Close()
-
-	// var game []models.Game
-	
-	// switch {
-	// 	case res.StatusCode == 401:
-	// 		c.JSON(http.StatusUnauthorized, gin.H{
-	// 			"error": "Not authorized",
-	// 		})
-	// 	case res.StatusCode >= 400:
-	// 		c.JSON(http.StatusBadRequest, gin.H{
-	// 			"error": "Bad request",
-	// 		})
-	// 	default:
-	// 		err := json.NewDecoder(res.Body).Decode(&game)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-
-	// 		c.JSON(http.StatusOK, gin.H{
-	// 			"response": game[0],
-	// 		})
-	// }
-	var game *[]models.Game
-	GetIGDBRoute(
-		c, 
-		"https://api.igdb.com/v4/games", 
-		"fields id, aggregated_rating, aggregated_rating_count, alternative_names, collections, cover, game_modes, genres, involved_companies, name, platforms, first_release_date, slug, summary, url", 
-		game,
-	)
 }
 
 func GetGameCover(c *gin.Context) {
